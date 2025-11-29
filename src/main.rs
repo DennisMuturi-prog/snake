a4b9ac06791c3f91da52da96f12b5479af702106
Dennis Muturi <deynohmuturi@gmail.com>
2025-11-29 18:02:14 +0300

    Refactor main.rs: Simplify imports, enhance setup logic, and add apple collision detection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use avian2d::
-    prelude::*
-;
+use avian2d::prelude::*;
 use bevy::{prelude::*, window::PrimaryWindow};
-use snake::fabrik::{JointFilter, Limb, LimbFilter};
+use rand::Rng;
+use snake::fabrik::{Joint, JointFilter, Limb, LimbFilter, LimbSegment};
 fn main() {
     App::new()
         .add_plugins((
@@ -13,6 +12,7 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(Update, follow_mouse)
         .add_systems(Update, move_snake)
+        .add_systems(Update, detect_collision_with_apple)
         .run();
 }
 
@@ -22,9 +22,12 @@ struct LimbResource(Limb);
 #[derive(Resource, Deref, DerefMut)]
 struct SnakeVelocity(Vec2);
 
-const SNAKE_SPEED:f32=10.0;
+#[derive(Component)]
+pub struct Apple;
 
-const NO_OF_SNAKE_PARTS:usize=8;
+const SNAKE_SPEED: f32 = 10.0;
+
+const NO_OF_SNAKE_PARTS: usize = 20;
 
 fn setup(
     mut commands: Commands,
@@ -36,53 +39,74 @@ fn setup(
     let color = Color::Srgba(Srgba::rgb(1.0, 0.647, 0.0));
     let material = materials.add(color);
     commands.spawn(Camera2d);
-    let limb = Limb::new(Vec2 { x: 200.0, y: 200.0 }, NO_OF_SNAKE_PARTS, Vec2 { x: 0.0, y: 0.0 });
+    let limb = Limb::new(
+        Vec2 { x: 200.0, y: 200.0 },
+        NO_OF_SNAKE_PARTS,
+        Vec2 { x: 200.0, y: -20.0 },
+    );
 
     limb.display(&mut commands, mesh, material);
 
     commands.insert_resource(LimbResource(limb));
 
-    commands.insert_resource(SnakeVelocity(Vec2 { x: 0.0, y: SNAKE_SPEED-5.0}));
-
+    commands.insert_resource(SnakeVelocity(Vec2 {
+        x: 0.0,
+        y: SNAKE_SPEED - 10.0,
+    }));
 
     let apple_shape = Circle::new(10.0);
-    let apple_mesh = meshes.add(shape);
+    let apple_mesh = meshes.add(apple_shape);
     let apple_color = Color::Srgba(Srgba::rgb(0.0, 0.647, 0.0));
 
-    let apple_material = materials.add(color);
+    let apple_material = materials.add(apple_color);
     commands.spawn((
-            Mesh2d(apple_mesh),
-            MeshMaterial2d(apple_material),
-            Transform::from_xyz(20.0, 50.0, 0.0),
-        ));
+        Mesh2d(apple_mesh),
+        MeshMaterial2d(apple_material),
+        Transform::from_xyz(20.0, 50.0, 0.0),
+        RigidBody::Kinematic,
+        Collider::circle(15.0),
+        Sensor,
+        Apple,
+    ));
 }
-
-
-
-
 
 fn move_snake(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    joint_query: Query<&mut Transform, JointFilter>,
-    limb_query: Query<&mut Transform, LimbFilter>,
+    joint_query: Query<(&mut Transform, &Joint), JointFilter>,
+    limb_query: Query<(&mut Transform, &LimbSegment), LimbFilter>,
     mut limb_resource: ResMut<LimbResource>,
     mut snake_velocity: ResMut<SnakeVelocity>,
 ) {
     if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        snake_velocity.0 = Vec2 { x: -SNAKE_SPEED, y: 0.0 };
+        snake_velocity.0 = Vec2 {
+            x: -SNAKE_SPEED,
+            y: 0.0,
+        };
     }
     if keyboard_input.pressed(KeyCode::ArrowRight) {
-        snake_velocity.0 = Vec2 { x: SNAKE_SPEED, y: 0.0 };
+        snake_velocity.0 = Vec2 {
+            x: SNAKE_SPEED,
+            y: 0.0,
+        };
     }
 
     if keyboard_input.pressed(KeyCode::ArrowUp) {
-        snake_velocity.0 = Vec2 { x: 0.0, y: SNAKE_SPEED };
+        snake_velocity.0 = Vec2 {
+            x: 0.0,
+            y: SNAKE_SPEED,
+        };
     }
 
     if keyboard_input.pressed(KeyCode::ArrowDown) {
-        snake_velocity.0 = Vec2 { x: 0.0, y: -SNAKE_SPEED };
-    }    
-    let target = limb_resource.get_last_segment_position()+snake_velocity.0;
+        snake_velocity.0 = Vec2 {
+            x: 0.0,
+            y: -SNAKE_SPEED,
+        };
+    }
+    if snake_velocity.0.length() == 0.0 {
+        return;
+    }
+    let target = limb_resource.get_last_segment_position() + snake_velocity.0;
     limb_resource.set_target(target);
     limb_resource.forward_fabrik();
     limb_resource.update_visuals(joint_query, limb_query);
@@ -91,8 +115,8 @@ fn follow_mouse(
     buttons: Res<ButtonInput<MouseButton>>,
     windows: Query<&Window, With<PrimaryWindow>>,
     camera: Query<(&Camera, &GlobalTransform)>,
-    joint_query: Query<&mut Transform, JointFilter>,
-    limb_query: Query<&mut Transform, LimbFilter>,
+    joint_query: Query<(&mut Transform, &Joint), JointFilter>,
+    limb_query: Query<(&mut Transform, &LimbSegment), LimbFilter>,
     mut limb_resource: ResMut<LimbResource>,
 ) -> Result {
     if buttons.pressed(MouseButton::Left) {
@@ -111,4 +135,17 @@ fn follow_mouse(
     }
 
     Ok(())
+}
+
+fn detect_collision_with_apple(
+    mut collision_reader: MessageReader<CollisionEnd>,
+    mut apple: Single<&mut Transform, With<Apple>>,
+) {
+    for event in collision_reader.read() {
+        let mut rng = rand::rng();
+        let x: f32 = rng.random_range(-300.0..=300.0);
+        let y: f32 = rng.random_range(-300.0..=300.0);
+        apple.translation.x = x;
+        apple.translation.y = y;
+    }
 }
